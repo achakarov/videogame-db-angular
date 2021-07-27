dbd36dc2ae1454e63d623d860059ee83954563d7
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-07-27 16:25:46 +0300

    updated Home component for showing all games

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -17,16 +16,24 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { HomeComponent } from './components/home/home.component';
 import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
 import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
+// import { DetailsComponent } from './components/details/details.component';
+// import { GameTabsComponent } from './components/game-tabs/game-tabs.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchBarComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    SearchBarComponent,
+    HomeComponent,
+    // DetailsComponent,
+    // GameTabsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    GaugeModule.forRoot(),
     HttpClientModule,
     FormsModule,
-    GaugeModule.forRoot(),
+    BrowserAnimationsModule,
     MatFormFieldModule,
     MatSelectModule,
     MatTabsModule,
