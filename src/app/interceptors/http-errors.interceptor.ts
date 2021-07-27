de89b26121202ae1cf612fecf4c75bdb0c01b255
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-07-27 16:48:09 +0300

    formatted the file properly

--- src/app/interceptors/http-errors.interceptor.ts
+++ src/app/interceptors/http-errors.interceptor.ts
@@ -1,16 +1,18 @@
 import {
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, throwError as observableThrowError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpRequest,
+  } from '@angular/common/http';
+  import { Injectable } from '@angular/core';
+  import { Observable, throwError as observableThrowError } from 'rxjs';
+  import { catchError } from 'rxjs/operators';
+
 
 @Injectable()
 export class HttpErrorsInterceptor implements HttpInterceptor {
   constructor() {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
